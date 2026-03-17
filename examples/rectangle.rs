62921a803e2fdaff32ec52f2b8411e2dfdca39fd
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-03-17 16:38:15 -0700

    feat: super basic first pass with an example

---
+++ examples/rectangle.rs
@@ -0,0 +1,57 @@
+use std::{io::Write, time::Duration};
+
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode, KeyEvent},
+    execute,
+    terminal::{self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tixel::{Color, HalfCellCanvas};
+
+fn main() -> anyhow::Result<()> {
+    let mut stdout = std::io::stdout();
+
+    let (cols, rows) = terminal::size()?;
+    let mut canvas = HalfCellCanvas::new(rows as usize, cols as usize);
+
+    let height = canvas.height();
+    let width = canvas.width();
+
+    terminal::enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen, Hide, Clear(ClearType::All))?;
+
+    let mut frame: u64 = 0;
+    let mut running = true;
+
+    loop {
+        if event::poll(Duration::from_millis(1))?
+            && let Event::Key(KeyEvent { code, .. }) = event::read()?
+        {
+            match code {
+                KeyCode::Char('q') => break,
+                KeyCode::Char(' ') => running = !running,
+                _ => {}
+            }
+        }
+
+        if running {
+            frame += 1;
+            for y in 0..height {
+                for x in 0..width {
+                    let r = (1. + (frame as f64 / 100.).cos()) / 2. * 256.;
+                    let g = (x as f64 / width as f64) * 256.;
+                    let b = (1. + (y as f64 / 2.).sin()) / 2. * 256.;
+                    canvas.set_color(x, y, Color::new(r as u8, g as u8, b as u8));
+                }
+            }
+        }
+
+        let output = canvas.render();
+        let _ = stdout.write(output.as_bytes());
+    }
+
+    execute!(stdout, LeaveAlternateScreen, Show)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,77 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+const UPPER_HALF_CELL: char = '▀';
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Color(u8, u8, u8);
+
+impl Color {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self(r, g, b)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// renders to strings using the half block character
+/// - allows setting color values
+/// - renders to a string that the caller can write to their screen
+pub struct HalfCellCanvas {
+    terminal_rows: usize,
+    terminal_cols: usize,
+    pixels: Vec<Vec<Option<Color>>>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl HalfCellCanvas {
+    pub fn new(terminal_rows: usize, terminal_cols: usize) -> Self {
+        let pixels = vec![vec![None; terminal_cols]; 2 * terminal_rows];
+
+        Self {
+            terminal_rows,
+            terminal_cols,
+            pixels,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.terminal_cols
+    }
+
+    pub fn height(&self) -> usize {
+        2 * self.terminal_rows
+    }
+
+    /// x and y are in canvas space, not terminal space
+    /// x is distance from left edge, y is distance from top
+    pub fn set_color(&mut self, x: usize, y: usize, color: Color) {
+        self.pixels[y][x] = Some(color)
+    }
+
+    pub fn render(&self) -> String {
+        let mut out = String::new();
+        out.push_str(&format!("\x1b[{};{}H", 0, 0));
+
+        for row in 0..self.terminal_rows {
+            for col in 0..self.terminal_cols {
+                let top_color = self.pixels[2 * row][col];
+                let bottom_color = self.pixels[2 * row + 1][col];
+
+                let fg_str = if let Some(top_color) = top_color {
+                    format!("\x1b[38;2;{};{};{}m", top_color.0, top_color.1, top_color.2)
+                } else {
+                    String::new()
+                };
+
+                let bg_str = if let Some(bottom_color) = bottom_color {
+                    format!(
+                        "\x1b[48;2;{};{};{}m",
+                        bottom_color.0, bottom_color.1, bottom_color.2
+                    )
+                } else {
+                    String::new()
+                };
+
+                let cell = format!("{fg_str}{bg_str}{UPPER_HALF_CELL}");
+                out.push_str(&cell);
+            }
+        }
+
+        out
     }
 }

ed2bb8b890e5ba0e7157418a5a49f63af7717573
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-03-31 14:42:24 -0700

    chore: add benches for rendering a solid color

--- benches/render.rs
+++ benches/render.rs
@@ -15,14 +15,43 @@ fn fill_checkerboard(canvas: &mut HalfCellCanvas) {
     }
 }
 
+fn fill_solid(canvas: &mut HalfCellCanvas) {
+    let color = Color::new(100, 200, 120);
+
+    for y in 0..canvas.height() {
+        for x in 0..canvas.width() {
+            canvas.set_color(x, y, color);
+        }
+    }
+}
+
 fn bench_render_frame(c: &mut Criterion) {
     let sizes = [("40x12", 40, 12), ("80x24", 80, 24), ("160x48", 160, 48)];
 
-    let mut group = c.benchmark_group("halfcell/render_frame");
+    let mut group = c.benchmark_group("halfcell/checkerboard/render_frame");
 
     for (name, cols, rows) in sizes {
         let mut canvas = HalfCellCanvas::new(rows, cols);
         fill_checkerboard(&mut canvas);
+
+        let output = canvas.render();
+
+        group.throughput(Throughput::Bytes(output.len() as u64));
+        group.bench_function(name, |b| {
+            b.iter(|| {
+                let o = canvas.render();
+                black_box(o);
+            })
+        });
+    }
+
+    group.finish();
+
+    let mut group = c.benchmark_group("halfcell/solid/render_frame");
+
+    for (name, cols, rows) in sizes {
+        let mut canvas = HalfCellCanvas::new(rows, cols);
+        fill_solid(&mut canvas);
 
         let output = canvas.render();
 
@@ -41,11 +70,31 @@ fn bench_render_frame(c: &mut Criterion) {
 fn bench_render_and_write(c: &mut Criterion) {
     let sizes = [("40x12", 40, 12), ("80x24", 80, 24), ("160x48", 160, 48)];
 
-    let mut group = c.benchmark_group("halfcell/render_and_write");
+    let mut group = c.benchmark_group("halfcell/checkerboard/render_and_write");
 
     for (name, cols, rows) in sizes {
         let mut canvas = HalfCellCanvas::new(rows, cols);
         fill_checkerboard(&mut canvas);
+
+        let output = canvas.render();
+        let mut file = File::create("/dev/null").unwrap();
+
+        group.throughput(Throughput::Bytes(output.len() as u64));
+        group.bench_function(name, |b| {
+            b.iter(|| {
+                let o = canvas.render();
+                file.write_all(o.as_bytes()).unwrap();
+            })
+        });
+    }
+
+    group.finish();
+
+    let mut group = c.benchmark_group("halfcell/solid/render_and_write");
+
+    for (name, cols, rows) in sizes {
+        let mut canvas = HalfCellCanvas::new(rows, cols);
+        fill_solid(&mut canvas);
 
         let output = canvas.render();
         let mut file = File::create("/dev/null").unwrap();

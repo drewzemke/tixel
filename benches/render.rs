6859f6def960201f52ac26c9f76aa9415619f021
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-04-01 10:49:17 -0700

    chore: add bench that changes render content each frame (to test delta rendering effectiveness)

--- benches/render.rs
+++ benches/render.rs
@@ -1,6 +1,7 @@
 use std::{fs::File, hint::black_box, io::Write};
 
 use criterion::{Criterion, Throughput, criterion_group, criterion_main};
+use rand::{SeedableRng, seq::IndexedRandom};
 use tixel::{Color, HalfCellCanvas};
 
 fn fill_checkerboard(canvas: &mut HalfCellCanvas) {
@@ -25,6 +26,15 @@ fn fill_solid(canvas: &mut HalfCellCanvas) {
     }
 }
 
+fn choose_pixels(canvas: &HalfCellCanvas, proportion: f32) -> Vec<(usize, usize)> {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(6);
+    let all_pixels: Vec<(usize, usize)> = (0..canvas.height())
+        .flat_map(|y| (0..canvas.width()).map(move |x| (x, y)))
+        .collect();
+    let n = (all_pixels.len() as f32 * proportion) as usize;
+    all_pixels.sample(&mut rng, n).cloned().collect()
+}
+
 fn bench_render_frame(c: &mut Criterion) {
     let sizes = [("40x12", 40, 12), ("80x24", 80, 24), ("160x48", 160, 48)];
 
@@ -38,6 +48,7 @@ fn bench_render_frame(c: &mut Criterion) {
 
         group.throughput(Throughput::Bytes(output.len() as u64));
         group.bench_function(name, |b| {
+            fill_checkerboard(&mut canvas);
             b.iter(|| {
                 let o = canvas.render();
                 black_box(o);
@@ -57,11 +68,41 @@ fn bench_render_frame(c: &mut Criterion) {
 
         group.throughput(Throughput::Bytes(output.len() as u64));
         group.bench_function(name, |b| {
+            fill_solid(&mut canvas);
             b.iter(|| {
                 let o = canvas.render();
                 black_box(o);
             })
         });
+    }
+
+    group.finish();
+
+    let mut group = c.benchmark_group("halfcell/changing/render_frame");
+
+    for (name, cols, rows) in sizes {
+        for proportion in [0.1, 0.5, 1.0] {
+            let mut canvas = HalfCellCanvas::new(rows, cols);
+
+            let target_pixels: Vec<(usize, usize)> = choose_pixels(&canvas, proportion);
+            let red = Color::new(255, 0, 0);
+            let blue = Color::new(0, 0, 255);
+
+            let mut flip = true;
+
+            group.bench_function(format!("{name}/{proportion}"), |b| {
+                b.iter(|| {
+                    let color = if flip { red } else { blue };
+                    for (x, y) in &target_pixels {
+                        canvas.set_color(*x, *y, color);
+                    }
+                    flip = !flip;
+
+                    let o = canvas.render();
+                    black_box(o);
+                })
+            });
+        }
     }
 
     group.finish();
@@ -82,6 +123,7 @@ fn bench_render_and_write(c: &mut Criterion) {
         group.throughput(Throughput::Bytes(output.len() as u64));
         group.bench_function(name, |b| {
             b.iter(|| {
+                fill_checkerboard(&mut canvas);
                 let o = canvas.render();
                 file.write_all(o.as_bytes()).unwrap();
             })
@@ -102,6 +144,7 @@ fn bench_render_and_write(c: &mut Criterion) {
         group.throughput(Throughput::Bytes(output.len() as u64));
         group.bench_function(name, |b| {
             b.iter(|| {
+                fill_solid(&mut canvas);
                 let o = canvas.render();
                 file.write_all(o.as_bytes()).unwrap();
             })

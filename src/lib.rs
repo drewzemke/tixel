70c470a5159a34b4515fd6f4646fafc21e9fe890
Drew Deleón-Zemke <drew.zemke@gmail.com>
2026-04-11 17:41:17 -0700

    chore: make HalfCellCanvas::new const

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ const UPPER_HALF_CELL: char = '▀';
 pub struct Color(u8, u8, u8);
 
 impl Color {
-    pub fn new(r: u8, g: u8, b: u8) -> Self {
+    pub const fn new(r: u8, g: u8, b: u8) -> Self {
         Self(r, g, b)
     }
 }
